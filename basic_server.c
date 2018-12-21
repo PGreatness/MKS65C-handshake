9d0804e170998ecbe075204dc6c9d0683007c630
Ahnaf Hasan <ahasan1@stuy.edu>
2018-12-20 23:17:23 -0500

    Add keyboard interrupt handling

--- basic_server.c
+++ basic_server.c
@@ -1,8 +1,14 @@
 #include "pipe_networking.h"
 
+void handle_sigint(int sig) {
+  printf("Goodbye\n");
+  remove("waluigi");
+  exit(1);
+}
 
 int main() {
 
+  signal(SIGINT, handle_sigint);
   int to_client;
   int from_client;
 
